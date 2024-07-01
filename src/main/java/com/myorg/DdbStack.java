3f13931ba387a48d32fdc4fa0cd10fcc181a1dba
Wagner Santos <wbatista985@gmail.com>
2024-07-01 19:57:00 -0300

    feat: add autoscaling tabela dynamoDB

--- src/main/java/com/myorg/DdbStack.java
+++ src/main/java/com/myorg/DdbStack.java
@@ -1,12 +1,10 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.dynamodb.Attribute;
-import software.amazon.awscdk.services.dynamodb.AttributeType;
-import software.amazon.awscdk.services.dynamodb.BillingMode;
-import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.*;
 import software.constructs.Construct;
 
 public class DdbStack extends Stack {
@@ -22,6 +20,8 @@ public class DdbStack extends Stack {
 
         productEventsDbd = Table.Builder.create(this, "productsEventsDb")
                 .tableName("product-events")
+                //para ser on demand a tabela PAY_PER_REQUEST
+                //.billingMode(BillingMode.PAY_PER_REQUEST)
                 .billingMode(BillingMode.PROVISIONED)
                 .readCapacity(1)
                 .writeCapacity(1)
@@ -36,6 +36,28 @@ public class DdbStack extends Stack {
                 .timeToLiveAttribute("ttl")
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
+
+        //configurando autoscaling da tabela DB , caso seja PAY_PER_REQUEST não é necessário
+        productEventsDbd.autoScaleReadCapacity(EnableScalingProps.builder()
+                .minCapacity(1)
+                .maxCapacity(4)
+                .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
+        productEventsDbd.autoScaleWriteCapacity(EnableScalingProps.builder()
+                        .minCapacity(1)
+                        .maxCapacity(4)
+                        .build())
+                .scaleOnUtilization(UtilizationScalingProps.builder()
+                        .targetUtilizationPercent(50)
+                        .scaleInCooldown(Duration.seconds(30))
+                        .scaleOutCooldown(Duration.seconds(30))
+                        .build());
+
 
     }
 
